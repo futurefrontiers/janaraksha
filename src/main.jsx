d2216149382c1c5cec3fe178b37837f3b1fc31c0
Madhu Jayavarapu <madhujayavarapu523@gmail.com>
2023-09-12 00:13:55 +0530

    move scss files into styles folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'aos/dist/aos.js';
 import 'glightbox/dist/js/glightbox.min.js';
-
-import './ui_fun';
-
-import App from './App.jsx';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
 import 'aos/dist/aos.css';
-
 import 'glightbox/dist/css/glightbox.min.css';
-
-import './index.scss';
+import './ui_fun';
+import './assets/styles/styles.scss';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
