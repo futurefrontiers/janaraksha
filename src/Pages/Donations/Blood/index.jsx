1fe453c1103b2a40c1672154b3b349738925a639
Anilvarmach <anilvarmach999@gmail.com>
2023-09-12 15:29:50 +0530

    Routes Implemented and removed Header from Home and renamed CommonHeader to Header

---
+++ src/Pages/Donations/Blood/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BloodDonation = () => {
+    return <div>BloodDonation</div>;
+};
+
+export default BloodDonation;
--- src/components/Breadcrumbs/index.jsx
+++ src/components/Breadcrumbs/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import breadcrumbs_img from '../../assets/images/breadcrumbs-bg.jpg';
 
-const Breadcrumbs = ({ Title, link }) => {
+const Breadcrumbs = ({ Title }) => {
     return (
         <div className='breadcrumbs d-flex align-items-center' style={{ backgroundImage: `url(${breadcrumbs_img})` }}>
             <div className='container position-relative d-flex flex-column align-items-center' data-aos='fade'>
                 <h2>{Title}</h2>
                 <ol>
                     <li>
-                        <a href='index.html'>Home</a>
+                        <Link to='/'>Home</Link>
                     </li>
                     <li>{Title}</li>
                 </ol>
