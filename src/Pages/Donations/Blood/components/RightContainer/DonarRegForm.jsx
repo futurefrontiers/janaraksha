f53ac9e8ae13defada1ee480dd602cefd3894e31
Madhu Jayavarapu <madhujayavarapu523@gmail.com>
2023-09-16 22:34:40 +0530

    donor registration form changes

--- src/Pages/Donations/Blood/components/RightContainer/DonarRegForm.jsx
+++ src/Pages/Donations/Blood/components/RightContainer/DonarRegForm.jsx
@@ -1,15 +1,8 @@
-import React, { useReducer } from 'react';
+import { useReducer, useState } from 'react';
+import { LocationFilters } from '../../../../../components';
 
-import { STATES_LIST, DISTRICTS, CITIES } from '../../../../../constants/addressOptions';
 import { BLOOD_GROUP_OPTIONS } from '../../../../../constants/bloodGroups';
 
-const initialState = {
-    districtList: [{ label: 'select District', value: '' }],
-    citiesList: [{ label: 'select City', value: '' }],
-    stateName: '',
-    district: '',
-    city: '',
-};
 
 const DonarFormReducer = (state, newState) => ({
     ...state,
@@ -17,33 +10,39 @@ const DonarFormReducer = (state, newState) => ({
 });
 
 const DonarRegForm = () => {
-    const [state, setState] = useReducer(DonarFormReducer, initialState);
-    const { districtList, citiesList, stateName, district, city } = state;
+    const [donor, setDonor] = useReducer(DonarFormReducer, {});
 
-    const handleStateChange = state => {
-        const dis = DISTRICTS[state];
-        setState({ stateName: state, districtList: [...districtList, ...dis] });
-    };
+    const handleSubmit = (ev) => {
+        ev.preventDefault();
+        console.log({
+            donor
+        })
+    }
 
-    const handleDistrictChange = dist => {
-        const cities = CITIES[dist];
-        setState({ district: dist, citiesList: [...citiesList, ...cities] });
-    };
+    const handleFormChange = (ev) => {
+        const value = ev.target.value
+        const name = ev.target.name
+        setDonor({[name]: value})
+    }
+
+    const handleLocationChange = (location) => {
+        setDonor(location)
+    }
 
     return (
         <div className='doner_form'>
-            <form action='forms/quote.php' method='post' className='php-email-form'>
+            <form className='php-email-form' onSubmit={() => handleSubmit()}>
                 <h3>Donar Registration</h3>
                 <p>
                     Please fill the following information to register as voluntary blood donor and become part of Janaraksha vision. Kindly update your date of donation once done, so that your name will be hidden
                     automatically till next 3 Months. Also please update your profile/information if in case you relocate in future.
                 </p>
-                <div className='row gy-3'>
+                <div className='row gy-3 mb-3'>
                     <div className='col-md-8'>
-                        <input type='text' name='name' className='form-control' placeholder='Name' required='' />
+                        <input type='text' name='name' className='form-control' placeholder='Name' onChange={handleFormChange} />
                     </div>
                     <div className='col-md-4'>
-                        <select className='form-select form-control' name='bloodgrp'>
+                        <select className='form-select form-control' name='blood_group'>
                             {BLOOD_GROUP_OPTIONS.map((grp, ind) => (
                                 <option key={ind} value={grp.value}>
                                     {grp.label}
@@ -52,50 +51,28 @@ const DonarRegForm = () => {
                         </select>
                     </div>
                     <div className='col-md-6'>
-                        <input type='number' className='form-control' name='phone' placeholder='Phone' required='' />
+                        <input type='number' className='form-control' name='phone' placeholder='Phone' onChange={handleFormChange} />
                     </div>
                     <div className='col-md-6 '>
-                        <input type='email' className='form-control' name='email' placeholder='Email' required='' />
+                        <input type='email' className='form-control' name='email' placeholder='Email' onChange={handleFormChange} />
                     </div>
 
                     <div className='col-md-6'>
-                        <input type='password' className='form-control' name='password' placeholder='Password' required='' />
+                        <input type='password' className='form-control' name='password' placeholder='Password' onChange={handleFormChange} />
                     </div>
                     <div className='col-md-6'>
-                        <input type='password' className='form-control' name='c_password' placeholder='Confirm Password' required='' />
+                        <input type='password' className='form-control' name='confirmPassword' placeholder='Confirm Password' onChange={handleFormChange} />
                     </div>
-                    <div className='col-md-4'>
-                        <select className='form-select form-control' value={stateName} name='state' onChange={e => handleStateChange(e.target.value)}>
-                            {STATES_LIST.map((state, ind) => (
-                                <option key={ind} value={state.value}>
-                                    {state.label}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className='col-md-4'>
-                        <select className='form-select form-control' name='district' onChange={e => handleDistrictChange(e.target.value)}>
-                            {districtList.map((dist, ind) => (
-                                <option key={ind} value={dist.value}>
-                                    {dist.label}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className='col-md-4'>
-                        <select className='form-select form-control' name='city'>
-                            {citiesList.map((city, ind) => (
-                                <option key={ind} value={city.value}>
-                                    {city.label}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
+                </div>
+
+                <LocationFilters onChange={handleLocationChange}/>
+
+                <div className="row">
                     <div className='col-md-12'>
                         <div className='form-check d-flex'>
-                            <input className='form-check-input' type='checkbox' />
+                            <input className='form-check-input' name="consent" type='checkbox' onChange={handleFormChange} />
                             <label className='form-check-label'>
-                                <p>I authorise this website to display my name and telephone number, so that the needy could contact me, as and when there is an emergency.</p>
+                                <p className='ml-4'>I authorise this website to display my name and telephone number, so that the needy could contact me, as and when there is an emergency.</p>
                             </label>
                         </div>
                     </div>
@@ -103,7 +80,7 @@ const DonarRegForm = () => {
                         <div className='loading'>Loading</div>
                         <div className='error-message' />
                         <div className='sent-message'>Your quote request has been sent successfully. Thank you!</div>
-                        <button type='submit'>Register</button>
+                        <button type='submit' onClick={handleSubmit}>Register</button>
                     </div>
                 </div>
             </form>
