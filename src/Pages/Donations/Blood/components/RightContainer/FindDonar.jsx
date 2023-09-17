288a663d8af6fdd0abb502ab4e12dead6a669972
Madhu Jayavarapu <madhujayavarapu523@gmail.com>
2023-09-17 19:46:54 +0530

    list of donors integration changes

--- src/Pages/Donations/Blood/components/RightContainer/FindDonar.jsx
+++ src/Pages/Donations/Blood/components/RightContainer/FindDonar.jsx
@@ -1,11 +1,14 @@
-import { LocationFilters } from '../../../../../components';
-import { BLOOD_GROUP_OPTIONS } from '../../../../../constants/bloodGroups';
+// import { LocationFilters } from '../../../../../components';
+// import { BLOOD_GROUP_OPTIONS } from '../../../../../constants/bloodGroups';
+import { useCollection } from '../../../../../hooks';
 
 
 const FindDonar = () => {
+    const { docs } = useCollection('donors');
+
     return (
         <>
-            <div className='doner_form'>
+            {/* <div className='doner_form'>
                 <form action='forms/quote.php' method='post' className='php-email-form php-email-form-filter'>
                     <h3>Find Donar</h3>
                     <div className='row gy-3'>
@@ -24,22 +27,26 @@ const FindDonar = () => {
                         </div>
                     </div>
                 </form>
-            </div>
+            </div> */}
             <div className='container ml-5'>
                 <table className='table table-hover'>
                     <thead>
                         <tr>
                             <th scope='col'>Name</th>
+                            <th scope='col'>Blood Group</th>
                             <th scope='col'>Mobile</th>
                             <th scope='col'>Email</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
+                        {docs.map(({data, id}) => (
+                            <tr key={id}>
+                                <td>{data.name}</td>
+                                <td>{data.blood_group}</td>
+                                <td>{data.phone || 'NA'}</td>
+                                <td>{data.email || 'NA'}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
