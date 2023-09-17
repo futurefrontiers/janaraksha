1343bf16015ab0b48d31df77a6b0c47edcb8b8f0
Madhu Jayavarapu <madhujayavarapu523@gmail.com>
2023-09-17 12:55:48 +0530

    find donor page refactoring chnages

--- src/Pages/Donations/Blood/components/RightContainer/FindDonar.jsx
+++ src/Pages/Donations/Blood/components/RightContainer/FindDonar.jsx
@@ -1,35 +1,8 @@
-import React, { useReducer } from 'react';
-
-import { STATES_LIST, DISTRICTS, CITIES } from '../../../../../constants/addressOptions';
+import { LocationFilters } from '../../../../../components';
 import { BLOOD_GROUP_OPTIONS } from '../../../../../constants/bloodGroups';
 
-const initialState = {
-    districtList: [{ label: 'Select District', value: '' }],
-    citiesList: [{ label: 'City', value: '' }],
-    stateName: '',
-    district: '',
-    city: '',
-};
-
-const FindDonarReducer = (state, newState) => ({
-    ...state,
-    ...newState,
-});
 
 const FindDonar = () => {
-    const [state, setState] = useReducer(FindDonarReducer, initialState);
-    const { districtList, citiesList, stateName, district, city } = state;
-
-    const handleStateChange = state => {
-        const dis = DISTRICTS[state];
-        setState({ stateName: state, districtList: [...districtList, ...dis] });
-    };
-
-    const handleDistrictChange = dist => {
-        const cities = CITIES[dist];
-        setState({ district: dist, citiesList: [...citiesList, ...cities] });
-    };
-
     return (
         <>
             <div className='doner_form'>
@@ -45,33 +18,7 @@ const FindDonar = () => {
                                 ))}
                             </select>
                         </div>
-                        <div className='col-md-3'>
-                            <select className='form-select form-control' value={stateName} name='state' onChange={e => handleStateChange(e.target.value)}>
-                                {STATES_LIST.map((state, ind) => (
-                                    <option key={ind} value={state.value}>
-                                        {state.label}
-                                    </option>
-                                ))}
-                            </select>
-                        </div>
-                        <div className='col-md-3'>
-                            <select className='form-select form-control' name='district' onChange={e => handleDistrictChange(e.target.value)}>
-                                {districtList.map((dist, ind) => (
-                                    <option key={ind} value={dist.value}>
-                                        {dist.label}
-                                    </option>
-                                ))}
-                            </select>
-                        </div>
-                        <div className='col-md-2'>
-                            <select className='form-select form-control' name='city'>
-                                {citiesList.map((city, ind) => (
-                                    <option key={ind} value={city.value}>
-                                        {city.label}
-                                    </option>
-                                ))}
-                            </select>
-                        </div>
+                        <LocationFilters />
                         <div className='col-md-1'>
                             <button type='submit'>Search</button>
                         </div>
@@ -79,7 +26,7 @@ const FindDonar = () => {
                 </form>
             </div>
             <div className='container ml-5'>
-                <table class='table table-hover'>
+                <table className='table table-hover'>
                     <thead>
                         <tr>
                             <th scope='col'>Name</th>
