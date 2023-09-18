41031e3ba10f2aa67ed84b398ab65346f783e6a7
Madhu Jayavarapu <madhujayavarapu523@gmail.com>
2023-09-18 13:39:00 +0530

    JRP: add filter support fo find-donors page

--- src/Pages/Donations/Blood/components/RightContainer/FindDonar.jsx
+++ src/Pages/Donations/Blood/components/RightContainer/FindDonar.jsx
@@ -1,19 +1,62 @@
-// import { LocationFilters } from '../../../../../components';
-// import { BLOOD_GROUP_OPTIONS } from '../../../../../constants/bloodGroups';
+import { LocationFilters } from '../../../../../components';
+import { BLOOD_GROUP_OPTIONS } from '../../../../../constants/bloodGroups';
+import { useEffect, useState } from 'react';
 import { useCollection } from '../../../../../hooks';
 
-
 const FindDonar = () => {
-    const { docs } = useCollection('donors');
+    const [bg, setBg] = useState('all')
+    const [location, setLocation] = useState({})
+    const { docs, setFilters } = useCollection('donors');
+
+    useEffect(() => {
+        refineSearch()
+    }, [])
+
+    const refineSearch = (ev) => {
+        if(ev && ev.preventDefault) {
+            ev.preventDefault();
+        }
+        const filters = []
+
+        // Filter donors who are ready to donate.
+        filters.push({
+            col: 'can_donate',
+            operator: '==',
+            value: true
+        })
+
+        // Add location filters if present.
+        if(location) {
+            if(location.state) {
+                filters.push({col: 'location.state', operator: '==', value: location.state})
+            }
+            if(location.district) {
+                filters.push({col: 'location.district', operator: '==', value: location.district})
+            }
+            if(location.city) {
+                filters.push({col: 'location.city', operator: '==', value: location.city})
+            }
+        }
+
+        // Add blood group filter
+        if(bg && bg != 'all') {
+            filters.push({col: 'blood_group', operator: '==', value: bg})
+        }
+        setFilters(filters)
+    }
+
+    function handleLocationChange(location) {
+        setLocation(location)
+    }
 
     return (
         <>
-            {/* <div className='doner_form'>
-                <form action='forms/quote.php' method='post' className='php-email-form php-email-form-filter'>
+            <div className='doner_form'>
+                <form className='php-email-form php-email-form-filter' onSubmit={refineSearch}>
                     <h3>Find Donar</h3>
                     <div className='row gy-3'>
                         <div className='col-md-3'>
-                            <select className='form-select form-control' name='bllod_group'>
+                            <select className='form-select form-control' name='bllod_group' onChange={e => setBg(e.target.value)}>
                                 {BLOOD_GROUP_OPTIONS.map((group, ind) => (
                                     <option key={ind} value={group.value}>
                                         {group.label}
@@ -21,13 +64,14 @@ const FindDonar = () => {
                                 ))}
                             </select>
                         </div>
-                        <LocationFilters />
+                        <LocationFilters onChange={handleLocationChange} />
                         <div className='col-md-1'>
-                            <button type='submit'>Search</button>
+                            <button type='submit' className='btn btn-primary'>Search</button>
                         </div>
                     </div>
                 </form>
-            </div> */}
+            </div>
+
             <div className='container ml-5'>
                 <table className='table table-hover'>
                     <thead>
